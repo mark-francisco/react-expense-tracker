a0299cfdbf15d51a55a0b7c728c76e29a39af179
Mark Francisco <mfrancisco581@gmail.com>
2021-08-25 14:20:28 -0500

    refactor App.js and Expenses.js to render ExpenseItems dynamically

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -17,10 +17,10 @@ function Expenses(props) {
       <Card className="expenses">
         <ExpenseFilter onChangeYear={filteredYearHandler} currentYear={filteredYear} />
 
-        <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount} />
-        <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount} />
-        <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount} />
-        <ExpenseItem date={props.items[3].date} title={props.items[3].title} amount={props.items[3].amount} />
+        {/* React can render an array of JSX elements */}
+        {props.items.map((expense) => {
+          return <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} />;
+        })}
       </Card>
     </div>
   );
